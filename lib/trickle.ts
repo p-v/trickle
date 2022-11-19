fc95291159d0e47ca2e73ae3781866258b83f093
Pratyush <pratyushverma@outlook.com>
2022-11-19 12:07:28 +0530

    Update trickle to use basic functions

--- lib/trickle.ts
+++ lib/trickle.ts
@@ -1,174 +1,68 @@
 import _ from "lodash";
+type Environment = { [key: string]: any };
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const globalEnvTemplateExpr = new RegExp("^{{(.*)}}$");
+const contextEnvTemplateExpr = new RegExp("^<<(.*)>>$");
 
-type Store = {
-  [key: string]: string | number | boolean;
-};
+export default class Trickle<X> {
+  private globals: Environment;
+  private context: Environment;
+  private ops: any[];
 
-type RetryConfig = {
-  retryCount?: number;
-  delayInterval?: number;
-};
-
-const DEFAULT_RETRY_COUNT = 5;
-const DEFAULT_DELAY_INTERVAL = 1000;
-
-const defaultRetryConfig = {
-  retryCount: DEFAULT_RETRY_COUNT,
-  delayInterval: DEFAULT_DELAY_INTERVAL,
-};
-
-const varTemplateExpr = new RegExp("^{{(.*)}}$");
-
-class Perform<Type> {
-  private dict: any;
-
-  constructor(dict: any) {
-    this.dict = dict;
-  }
-
-  getGlobalVal(key: string) {
-    return _.get(this.dict.g, key);
+  constructor(globals: Environment, context: Environment, ops: any[] = []) {
+    this.globals = globals;
+    this.context = context;
+    this.ops = ops;
   }
 
   private resolveArgs(args: any[]) {
     return args.map((arg) => {
       if (typeof arg === "string") {
-        const match = varTemplateExpr.exec(arg);
+        let match = globalEnvTemplateExpr.exec(arg);
         if (match) {
-          return _.get(this.dict.g, match[1]);
+          return _.get(this.globals, match[1]);
+        }
+        match = contextEnvTemplateExpr.exec(arg);
+        if (match) {
+          return _.get(this.context, match[1]);
         }
       }
       return arg;
     });
   }
 
-  peek(fn: (x: Type) => any, name: string = "peek"): Perform<Type> {
-    const op = (args: Type) => {
-      console.log(args);
-      fn(args);
-      return args;
-    };
-    this.dict.ops.push({ op, name });
-    return this;
+  new<N>(func: (...x: any) => N | Promise<N>, args: Parameters<typeof func>) {
+    this.ops.push(() => func(...this.resolveArgs(args)));
+    return new Trickle<N>(this.globals, this.context, this.ops);
   }
 
-  perform<N>(
-    fn: (...x: any) => N | PromiseLike<N>,
-    args: Parameters<typeof fn>,
-    name = "perform"
-  ): Perform<N> {
-    const op = (prev: any) =>
-      prev ? fn.call(null, prev) : fn(...this.resolveArgs(args));
-    this.dict.ops.push({ op, name });
-    return new Perform<N>(this.dict);
-  }
-
-  performNew<N>(
-    fn: (...x: any) => N | PromiseLike<N>,
-    args: Parameters<typeof fn>,
-    name: string = "performNew"
-  ): Perform<N> {
-    const op = () => fn(...this.resolveArgs(args));
-    this.dict.ops.push({ op, name });
-    return new Perform<N>(this.dict);
-  }
-
-  performUntil<N>(
-    fn: (...x: any) => N | PromiseLike<N>,
-    args: Parameters<typeof fn>,
-    retryFn: (_: N) => boolean,
-    retryConfig: RetryConfig = defaultRetryConfig,
-    name: string = "performUntil"
-  ) {
-    const op = async () => {
-      const { retryCount, delayInterval } = {
-        ...defaultRetryConfig,
-        ...retryConfig,
-      };
-      const resArgs = this.resolveArgs(args);
-      let res = await Promise.resolve(fn(...resArgs));
-      let retryN = 0;
-      let interval = delayInterval;
-      while (!(await Promise.resolve(retryFn(res)))) {
-        await delay(interval);
-        interval = 2 * interval;
-        res = await Promise.resolve(fn(...resArgs));
-        retryN++;
-        if (retryN >= retryCount) {
-          throw new Error("Condition timeout");
-        }
-      }
-
-      return res;
-    };
-    this.dict.ops.push({ op, name });
-    return new Perform<N>(this.dict);
-  }
-
-  transform<Output>(
-    fn: (x: Type) => Output | PromiseLike<Output>,
-    name = "transform"
-  ): Perform<Output> {
-    const op = (args: Type) => fn(args);
-    this.dict.ops.push({ op, name });
-    return new Perform<Output>(this.dict);
-  }
-
-  globalArgs(
-    from: string,
-    to: string,
-    name: string = "globalArgs"
-  ): Perform<Type> {
-    const op = (obj: Type) => {
-      _.set(this.dict.g, to, _.get(obj, from));
-    };
-    this.dict.ops.push({ op, name });
-    return this;
-  }
-
-  validate(fn: (_: Type) => boolean, name?: string): Perform<Type> {
-    name = name || "validate";
-    const op = (args: any) => {
-      if (!fn(args)) {
-        throw new Error("Validation error");
-      }
-      return true;
-    };
-    this.dict.ops.push({ op, name });
-    return this;
-  }
-
-  async done(): Promise<void> {
-    let prev = null;
-    for (const opr of this.dict.ops) {
-      const { op, name } = opr;
-      console.log("Executing: " + name);
-      prev = await Promise.resolve(op(prev));
-      console.log("Result: " + prev);
-    }
-    console.log("All operations completed");
-  }
-}
-
-export default class Trickle {
-  private constructor() {}
-
-  public static withStore(store: Store) {
-    return new Perform({
-      ops: [],
-      g: store,
+  store(fn: (x: X) => { [key: string]: any }) {
+    this.ops.push((obj: X) => {
+      let x = fn(obj);
+      Object.keys(x).forEach((k) => (this.context[k] = x[k]));
+      return obj;
     });
+    return this;
   }
 
-  public static perform<T>(
-    fn: (...x: any) => T | PromiseLike<T>,
-    args: Parameters<typeof fn>,
-    dict: any = { ops: [], g: {} },
-    name: string = "perform"
-  ): Perform<T> {
-    const perf = new Perform<T>(dict);
-    return perf.perform(fn, args, name);
+  continue(func: (x: X) => void) {
+    this.ops.push((prev: any) => {
+      func(prev);
+      return prev;
+    });
+    return this;
+  }
+
+  transform<N>(func: (x: X) => N) {
+    this.ops.push((prev: any) => func(prev));
+    return new Trickle<N>(this.globals, this.context, this.ops);
+  }
+
+  async done() {
+    let prev = null;
+    for (let op of this.ops) {
+      prev = await Promise.resolve(op(prev));
+    }
+    return prev;
   }
 }
--- tests/trickle.spec.ts
+++ tests/trickle.spec.ts
@@ -1,38 +1,58 @@
 import Trickle from "../lib/trickle";
-import chai, { expect } from "chai";
-import chaiAsPromised from "chai-as-promised";
-
-chai.use(chaiAsPromised);
+import { expect } from "chai";
+import sinon from "sinon";
+import { once } from "lodash";
 
 describe("On trickle", () => {
   it("perform single operation", async () => {
-    const store = { ops: [], g: {} };
-    await Trickle.perform(() => 3, [], store).done();
-    expect(store.ops.length).to.be.equal(1);
+    const ops: any[] = [];
+    const trickle = new Trickle({}, {}, ops);
+    await trickle.new(() => 3, []).done();
+    expect(ops.length).to.be.equal(1);
   });
 
-  it("throw exception when validate fails", async () => {
-    await expect(
-      Trickle.perform(() => 3, [])
-        .validate((x) => x === 4)
-        .done()
-    ).to.be.rejected;
+  it("perform multi operation", async () => {
+    const ops: any[] = [];
+    const trickle = new Trickle({}, {}, ops);
+    await trickle
+      .new(() => 3, [])
+      .continue(() => 3)
+      .done();
+    expect(ops.length).to.be.equal(2);
   });
 
-  it("finish when validate pass", async () => {
-    await expect(
-      Trickle.perform(() => 3, [])
-        .validate((x) => x === 3)
-        .done()
-    ).to.not.be.rejected;
+  it("use environment variable", async () => {
+    const ops: any[] = [];
+    const environment = {
+      env: "production",
+    };
+    const trickle = new Trickle(environment, {}, ops);
+    const cb = sinon.fake();
+    const proxy = once(cb);
+    await trickle
+      .new((x, y) => x + ":" + y, ["{{env}}", 3])
+      .continue((x) => {
+        proxy(x);
+      })
+      .done();
+    expect(cb.calledWith("production:3")).to.be.true;
   });
 
-  it("with store perform operation", async () => {
-    await expect(
-      Trickle.withStore({ env: "prod" })
-        .performNew((x, y) => x + ":" + y, ["{{env}}", 3])
-        .validate((x) => x === "prod:3")
-        .done()
-    ).to.not.be.rejected;
+  it("store and use context argument", async () => {
+    const ops: any[] = [];
+    const environment = {
+      env: "production",
+    };
+    const context = {};
+    const trickle = new Trickle(environment, context, ops);
+    const cb = sinon.fake.returns("ok");
+    const proxy = once(cb);
+
+    await trickle
+      .new((x, y) => x + ":" + y, ["{{env}}", 3])
+      .store((x) => ({ res: x }))
+      .new((x) => proxy(x), ["<<res>>"])
+      .done();
+    expect(cb.calledWith("production:3")).to.be.true;
   });
 });
